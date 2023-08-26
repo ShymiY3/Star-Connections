a0b8192e862416c96106d6ddab1d699767cd1b3f
root <root@HK-0212>
2023-08-26 19:32:49 +0200

    Added chunk progress

--- database/parser.py
+++ database/parser.py
@@ -1,48 +1,66 @@
 import pandas as pd
-import numpy as np
 import os
 import functools
 import math
 import multiprocessing as mp
+import time
 from multiprocessing.managers import BaseManager
 from database import DATABASE_URL
 from models import Movies
 from typing import Iterable, Callable
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import time
+from itertools import repeat
 
 
 def print_status(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        start = time.time_ns()
         message = f'{kwargs.get("file")}: Parsing data from file'
         print(message, flush=True)
         result = func(*args, **kwargs)
+        print(f'\nRuntime of function {func.__name__}: {(time.time_ns() - start)/1e9:.3f}')
         print(f'{kwargs.get("file")}: Data parsed from file')
         return result
-
     return wrapper
+
+class Counter(object):
+    def __init__(self, initval=0):
+        self.val = mp.Value('i', initval)
+        self.lock = mp.Lock()
+
+    def value(self):
+        with self.lock:
+            return self.val.value
+    
+    def increment(self):
+        with self.lock:
+            self.val.value += 1
 
 
 class Parser:
     class CustomManager(BaseManager):
         pass
-
+    
+    
     def __init__(self, db_url) -> None:
         self.db_url = db_url
         self.data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
         self.CustomManager.register("set", set)
+        self.CustomManager.register("Counter", Counter)
         self.title_filter = set()
         self.name_filter = set()
-
+        self.current_chunk = 0
+        self.num_records = 0
+        
     def chunk_loader(
         self, file: str, cols: Iterable | None = None, chunksize: int = 1000
     ):
         for chunk in pd.read_csv(
             os.path.join(self.data_dir, file),
             delimiter="\t",
-            chunksize=1000,
+            chunksize=chunksize,
             na_values="\\N",
             usecols=cols,
         ):
@@ -61,7 +79,17 @@ class Parser:
 
     def get_movies(self):
         _, session = self.connect_db()
-        return set(map(lambda x: x[0], session.query(Movies.id).all()))
+        x = set(map(lambda x: x[0], session.query(Movies.id).all()))
+        with open('newfile3', 'w') as f:
+            f.write(','.join(x))
+    
+    def print_chunk_info(self, size):
+        self.current_chunk.increment()
+        message = f"Processing chunk: {self.current_chunk.value()}/{math.ceil(self.num_records/size)}"
+        print("\r" + " " * len(message), end="", flush=True)
+        print(f"\r{message}" + ' '*6, end="", flush=True)
+        
+    
     
     @print_status
     def multi_process(
@@ -72,40 +100,23 @@ class Parser:
         num_processes: int = mp.cpu_count(),
         cols: Iterable | None = None,
     ):
-        start = time.time_ns()
-        # file_path = os.path.join(self.data_dir, file)
-        # count = 0
-
-        # with open(file_path, encoding="UTF-8") as f:
-        #     num_records = sum(1 for _ in f) - 1
-
-        # chunksize *= num_processes
+        file_path = os.path.join(self.data_dir, file)
         
+        with open(file_path, encoding='UTF-8') as f:
+            self.num_records = sum(1 for _ in f)
 
-        # with mp.Pool(processes=num_processes) as pool:
-        #     for chunk in self.chunk_loader(file, cols, chunksize):
-        #         message = f"Processing chunk: {count+1}/{math.ceil(num_records/chunksize)}"
-        #         print("\r" + " " * len(message), end="", flush=True)
-        #         print("\r", message, end="", flush=True)
-        #         pool.map(func, np.array_split(chunk, num_processes))
-        #         count += 1
-        #     else:
-        #         print()
-        #print(f'\nRuntime of function {__name__}: {(time.time_ns() - start)/10e9:.3f}')
-        start = time.time_ns()
         with mp.Pool(processes=num_processes) as pool:
-            pool.map(func, self.chunk_loader(file, cols, chunksize), num_processes)
-        print(f'\nRuntime of function {__name__}: {(time.time_ns() - start)/10e9:.3f}')
+            pool.map(func, self.chunk_loader(file, cols, chunksize))
     # -----------------------------------------
     # LOADING FUNCTIONS
     # -----------------------------------------
-    def load_title_basics(self, chunk: pd.DataFrame):
+    def load_title_basics(self, chunk: pd.DataFrame, ):
         column_mapping = {
             "tconst": "id",
             "originalTitle": "original_title",
             "startYear": "start_year",
         }
-
+        self.print_chunk_info(chunk.shape[0])
         chunk_copy = chunk.rename(columns=column_mapping)
         movie_filter = (
             (chunk_copy["titleType"] == "movie")
@@ -127,6 +138,7 @@ class Parser:
             "tconst": "movie_id",
             "nconst": "actor_id",
         }
+        self.print_chunk_info(chunk.shape[0])
 
     def load_name_basics(self, chunk: pd.DataFrame):
         column_mapping = {
@@ -135,6 +147,7 @@ class Parser:
             "birthYear": "birth_year",
             "deathYear": "death_year",
         }
+        self.print_chunk_info(chunk.shape[0])
 
     def load_ratings(self, chunk: pd.DataFrame):
         column_mapping = {
@@ -142,16 +155,13 @@ class Parser:
             "averageRating": "average",
             "numVotes": "num_votes",
         }
+        self.print_chunk_info(chunk.shape[0])
         chunk_copy = chunk.rename(columns=column_mapping)
         chunk_copy.dropna(how="any", inplace=True)
         ratings_filter = chunk_copy["movie_id"].isin(self.title_filter)
         chunk_copy = chunk_copy[ratings_filter]
-        print('\r', ''*30, end='', flush=True)
-        print(f'\rLoading records number: {chunk_copy.shape[0]}', end='', flush=True)
         if not chunk_copy.empty:
             self.load_to_db(chunk_copy, "ratings")
-
-
         
     def run(self):
         errors = []
@@ -160,7 +170,7 @@ class Parser:
         with self.CustomManager() as manager:
             self.title_filter = manager.set()
             self.name_filter = manager.set()
-
+            self.current_chunk = manager.Counter(0)
             # func = self.load_title_basics
             # file = "title_basics.tsv"
             # try:
@@ -175,6 +185,7 @@ class Parser:
             #     errors.append((func.__name__, file, e))
             #     print(f"\n{file}: Failed to parse from file")
             self.title_filter = self.title_filter._getvalue()
+            self.current_chunk = manager.Counter(0)
             if title_flag:
                 self.title_filter = self.get_movies()
                 if not self.title_filter:
@@ -211,10 +222,11 @@ class Parser:
 
             # RATINGS
             self.name_filter = self.name_filter._getvalue()
+            self.current_chunk = manager.Counter(0)
             func = self.load_ratings
-            file = "title_ratings.tsv"
+            file = "newfile.tsv"
             try:
-                self.multi_process(func=func, file=file)
+                self.multi_process(func=func, file=file, chunksize=500)
             except Exception as e:
                 errors.append((func.__name__, file, e))
                 print(f"\n{file}: Failed to parse from file")
@@ -264,5 +276,6 @@ class Parser:
 
 if __name__ == "__main__":
     p = Parser(DATABASE_URL)
-    p.run()
+    # p.run()
     # Parser.parse(DATABASE_URL)
+    p.get_movies()
